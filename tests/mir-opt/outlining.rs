12508b0da29facfcd0fb3b098033374cf91d8979
Johann Hemmann <johann.hemmann@code.berlin>
2023-05-23 19:57:14 -0300

    Bless mir dump

--- tests/mir-opt/outlining.rs
+++ tests/mir-opt/outlining.rs
@@ -1,14 +1,34 @@
 // unit-test: InstSimplify
 
-// EMIT_MIR outlining.opt1.InstSimplify.diff
-fn opt1(x: bool) -> u32 {
-    if x != true {
-        0
-    } else {
-        1
+// EMIT_MIR outlining.foo.InstSimplify.diff
+fn foo(x: impl Into<Bar>) {
+    let mut x: Bar = x.into();
+
+    // non-generic code
+    x.incr();
+}
+
+struct Bar(u32);
+
+impl Bar {
+    fn incr(&mut self) {
+        self.0 += 1;
+    }
+}
+
+impl From<u32> for Bar {
+    fn from(value: u32) -> Self {
+        Self(value)
+    }
+}
+
+impl From<u16> for Bar {
+    fn from(value: u16) -> Self {
+        Self(value as _)
     }
 }
 
 fn main() {
-    opt1(false);
+    foo(1_u32);
+    foo(2_u16);
 }
